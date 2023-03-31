7c7b21dc6795cde12a96d9aa36b66851a1f020ba
jloprad <124887387+jloprad@users.noreply.github.com>
2023-03-31 09:33:43 -0300

    changing metrics log to txt

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,7 +1,7 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
 
-
-# Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
     """
     Trains a machine learning model and returns it.
@@ -18,7 +18,20 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    rfc = RandomForestClassifier(random_state=24)
+
+    param_grid = {
+    'n_estimators': [50, 100],
+    'max_features': ['sqrt'],
+    'max_depth': [4, 5, 100],
+    'criterion': ['gini', 'entropy']
+    }
+
+    cv_rfc = GridSearchCV(estimator=rfc, param_grid=param_grid, cv=5)
+    cv_rfc.fit(X_train, y_train)
+    rf = cv_rfc.best_estimator_
+
+    return rf
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +70,39 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    predictions = model.predict(X)
+    return predictions
+
+def compute_model_metrics_slice(x, y, preds, cat_features ):
+    """
+    Calculates the model metrics for all the slices of categorical variables
+
+    Inputs
+    ------
+    x : DataFrame
+        Original dataframe, before one hot encoding
+    y : np.array
+        Known labels, binarized.
+    preds : np.array
+        Predicted labels, binarized.
+    Returns
+    -------
+    precision : float
+    recall : float
+    fbeta : float
+    """
+
+    # Only works for categorical variables
+    f = open("myfile.txt", "w")
+
+    for cat in cat_features:
+        print(f"Variable: {cat}")
+        for value in x[cat].unique():
+            print(f" Slice: {value}")
+            mask = (x[cat]==value)
+            precision, recall, fbeta = compute_model_metrics( y[mask] , preds[mask] )
+            print(f"  Precision: {precision}")
+            print(f"  Recall: {recall}")
+            print(f"  Fbeta: {fbeta}")
+
+    return None
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,13 +1,23 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+from ml.data import process_data
+import joblib
+from ml.data import process_data ########### PATH
+from ml.model import * ########### PATH
+import pandas as pd
 
-# Add the necessary imports for the starter code.
 
 # Add code to load in the data.
 
+df = pd.read_csv('../data/census.csv')
+
+# Some data cleaning
+df.columns = df.columns.str.strip()
+df = df.drop_duplicates()
+
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
-train, test = train_test_split(data, test_size=0.20)
+train, test = train_test_split(df, test_size=0.20, random_state=24)
 
 cat_features = [
     "workclass",
@@ -19,10 +29,21 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, __, __ = process_data(
+    test, categorical_features=cat_features,label='salary', training=False, encoder=encoder, lb=lb
+)
 
 # Train and save a model.
+rf = train_model(X_train, y_train)
+
+pred = inference(rf, X_test)
+compute_model_metrics_slice(test, y_test, pred)
+
+joblib.dump( rf, '../model/rf_model.pkl')
