c493a2ac6c3261ec71f4b9cc267f321bb9ab26b8
jloprad <124887387+jloprad@users.noreply.github.com>
2023-04-05 09:52:00 -0400

    added live post test and docstrings

--- starter/live_post.py
+++ starter/live_post.py
@@ -6,24 +6,24 @@ import json
 
 # With this input the expected output is: 'Salary is >50K'
 data = {
-  "age": 52,
-  "workclass": "Self-emp-inc",
-  "fnlgt": 287927,
-  "education": "HS-grad",
-  "education-num": 9,
-  "marital-status": "Married-civ-spouse",
-  "occupation": "Exec-managerial",
-  "relationship": "Wife",
-  "race": "White",
-  "sex": "Female",
-  "capital-gain": 15024,
-  "capital-loss": 0,
-  "hours-per-week": 40,
-  "native-country": "United-States"
-}
+    "age": 52,
+    "workclass": "Self-emp-inc",
+    "fnlgt": 287927,
+    "education": "HS-grad",
+    "education-num": 9,
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Exec-managerial",
+    "relationship": "Wife",
+    "race": "White",
+    "sex": "Female",
+    "capital-gain": 15024,
+    "capital-loss": 0,
+    "hours-per-week": 40,
+    "native-country": "United-States"
+    }
 
 r = requests.post("https://model-deployment-project.onrender.com/model_inference/", data=json.dumps(data))
 
 # Print results
-print( f"Status code returned: {r.status_code}" )
-print( r.json() )
+print(f"Status code returned: {r.status_code}")
+print(r.json())
